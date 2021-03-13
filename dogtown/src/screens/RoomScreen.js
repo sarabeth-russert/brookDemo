a9d81fc0c6a7b499192482ef2c03ea2a4ae0e29b
SARA RUSSERT <sarabeth.russert@gmail.com>
2021-03-12 20:33:08 -0800

    removes all dogs

--- dogtown/src/screens/RoomScreen.js
+++ dogtown/src/screens/RoomScreen.js
@@ -10,16 +10,28 @@ import CardDeck from 'react-bootstrap/CardDeck'
 
 const RoomScreen = () => {
   const [dogs, setDogs] = useState({
-    Baxter: {name: 'Baxter', breed: 'Lab', keyCareInfo: 'Loves pets!'}, 
-    Mario: {name: 'Mario', breed: 'Boston', keyCareInfo: 'Plays with slippers'}, Indiana: {name: 'Indiana', breed: 'Bouvier', keyCareInfo: 'Loves Cookies'}, 
-    Mj: {name: 'Mj', breed: 'Schnauzer', keyCareInfo: 'Loves Cookies'}})
+    Baxter: {name: 'Baxter', breed: 'Lab', keyCareInfo: 'Loves pets!', room: 'none'}, 
+    Mario: {name: 'Mario', breed: 'Boston', keyCareInfo: 'Plays with slippers', room: 'none'}, Indiana: {name: 'Indiana', breed: 'Bouvier', keyCareInfo: 'Loves Cookies', room: 'none'}, 
+    Mj: {name: 'Mj', breed: 'Schnauzer', keyCareInfo: 'Loves Cookies', room: 'none'}})
   const [rooms, setRooms] = useState([
-    {name: 'Big Dogs', occupancy: 10, dogs: [], image: './bigDog.jpg'},
-    {name: 'Small Dogs', occupancy: 15, dogs: [dogs.Mj], image: './smallDog.jpg'},
-    {name: 'Kennel', occupancy: 20, dogs: [dogs.Indiana], image: './kennel.jpg'},
-    {name: 'Walks', occupancy: 3, dogs: [dogs.Mario, dogs.Baxter], image: './walk.jpg'}]);
+    {name: 'Big Dogs', occupancy: 10, dogs: [dogs.Baxter], image: './bigDog.jpg'},
+    {name: 'Small Dogs', occupancy: 15, dogs: [], image: './smallDog.jpg'},
+    {name: 'Kennel', occupancy: 20, dogs: [], image: './kennel.jpg'},
+    {name: 'Walks', occupancy: 3, dogs: [], image: './walk.jpg'}]);
 
-    const addDog = (dog, room) => {
+    const addDog = (pup, room) => {
+      let map = {};
+      for (let i = 0; i < rooms.length; i++) {
+        map[rooms[i].name] = rooms[i];
+        map[rooms[i].name].dogs = [];
+      }
+      
+      let temp = [];
+      for (let room in map) {
+        temp.push(map[room]);
+      }
+      setRooms(temp);
+      
       
     }
 
